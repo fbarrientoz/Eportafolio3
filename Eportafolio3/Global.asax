9b50b6e0c4dc2e4d37ca21924b4e701384423241
Fabiola <Fabiola@LAPTOP-5NFTL38M>
2023-11-23 18:32:44 -0800

    Add project files.

---
+++ Eportafolio3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eportafolio3.MvcApplication" Language="C#" %>
---
+++ Eportafolio3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eportafolio3.Startup))]
+namespace Eportafolio3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
